565b92143c2b8a8355b5c29dfb32e8bd70b3cc4f
Your Namerafael <you@example.comrafaeldesousacavalcante21@gmail.com>
2024-10-13 14:22:56 -0300

    mostrando dados sql

--- backend/mostrar.php
+++ backend/mostrar.php
@@ -1,14 +1,32 @@
 <?php
-include("../../../back/models/conexao.php");
+include("../db/conexao.php");
 
 /**forma de mostrar todos os registros da tabela sql */
-/*
-    $variavel =  $sql_query->fetch_assoc();
-    $_SESSION["id"] = $variavel["id"];
-    $_SESSION["email"] = $variavel["email"];
-    $variavel["email"];
-    foreach ($variavel as $teste) {
-        echo $teste;
+
+
+
+/**verificando se a conexão foi */
+if($mysqli -> connect_errno){
+    echo"deu nao mano",$mysqli -> connect_errno,$mysqli -> connect_error;
+}
+else {
+    $sql_cod = "SELECT * FROM pessoas";
+    /**utilizando parametro query para enviar codigo sql caso nao funcione die */
+    $sql_query = $mysqli -> query($sql_cod) or die("voce simplismente nao existe");
+    /**verificando se teve algum retorno do meu select*/   
+    $quantidade = $sql_query->num_rows;
+    $loop = 0;
+    /**variavael ira guardar dados do banco de dados como uma array*/
+    while ($loop < $quantidade) {
+        /**variavael ira guardar dados do banco de dados como uma array*/
+        $variavel =  $sql_query->fetch_assoc();  
+        foreach ($variavel as $teste) {
+            echo $teste, "<br>";
+        }
+        $loop += 1;
+        echo"<hr>";
     }
-*/
+    
+}
+
 ?>
