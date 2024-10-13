7ebe27a09a013d60cfb2dd65954945dcf39a626a
Your Namerafael <you@example.comrafaeldesousacavalcante21@gmail.com>
2024-10-12 22:28:27 -0300

    teste

--- bd_carla.sql
+++ bd_carla.sql
@@ -4,7 +4,6 @@ create table login(id int primary key auto_increment not null,email varchar(50) 
 insert into login
 value(null,"rafaelvon514@gmail.com",12345678);
 
-
 select * from login;
 
 select * from login where email = "rafaelvon514@gmail.com" and senha = 12345678;
