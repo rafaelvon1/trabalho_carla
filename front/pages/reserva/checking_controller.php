ac6a9f4fbaf75b4814ad2721adcad441e4f7c032
Your Namerafael <you@example.comrafaeldesousacavalcante21@gmail.com>
2024-10-19 01:48:11 -0300

    melhorando estrutura

---
+++ front/pages/reserva/checking_controller.php
@@ -0,0 +1,19 @@
+<?php
+include("../../../db/conexao.php");
+/**pegando id da tela de login*/
+$id = $_SESSION["id"];
+/**----essa parte ira fazer a verificaçao se dados existem para nao aparecer na tela do layout------- */
+
+$sql_code = "SELECT * FROM reserva where id_client = $id";
+/** utilizando um parametro para query para rodar meu codigo no banco de dados caso der erro aparece a mensagem (die->) -> aqui se espera que algo seja retornado*/
+$sql_query = $mysqli -> query($sql_code) or die("voce simplismente nao existe");
+$pull =$sql_query->num_rows;
+/**verifico se meu usuario ja existe no banco do registro, permitido apenas 1 reserva */
+if ($pull == 0){
+    /**nao faz nada */
+}
+else {
+    /**me leva para a pagina onde mostra minha reserva */
+    header("location: mostrando_reserva.php");
+}
+?>
