f35d82405bacc6a9ae424c52bd0499592e150aff
Gabriel FV <gabrielvasco906@gmail.com>
2024-11-05 22:36:37 -0300

    removendo o style do php e criando o arquivo css

--- front/pages/reserva/reserva.php
+++ front/pages/reserva/reserva.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="..\imagens_videos" type="image/x-icon">
     <link rel="shortcut icon" href="..\imagens_videos\pizza_reserva.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../assets/styles/reserva.css">
     <title>reserva</title>
     <?php
         /**dados na posição 4 = id do cliente 
@@ -19,88 +22,7 @@
         include("checking_controller.php");
     ?>
 </head>
-<style>
-.cabecalho {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: black;
-}
 
-.nav {
-    display: flex;
-    width: 100%;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-}
-
-.nav-item {
-    margin-right: 20px; /* Espaçamento entre os itens */
-}
-
-.nav-item.sair {
-    margin-left: auto; /* Move o item 'sair' para o final da linha */
-}
-.nav-link {
-    text-align: center;
-}
-a {
-    /*font :)*/
-    font-family: 'Courier New', Courier, monospace;
-    color: #8f8c04; /* Cor do texto */
-}
-.video-background {
-    position: relative;
-    width: 100%;
-    height: 100vh; /* Define a altura do vídeo como a altura total da tela */
-    overflow: hidden;
-}
-
-.video-background video {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    min-width: 100%; /* Garante que o vídeo cubra toda a largura */
-    min-height: 100%; /* Garante que o vídeo cubra toda a altura */
-    width: auto;
-    height: auto;
-    transform: translate(-50%, -50%);
-    z-index: -1; /* Coloca o vídeo atrás do conteúdo */
-}
-
-.conteudo {
-    position: relative;
-    z-index: 1; /* Garante que o conteúdo fique na frente do vídeo */
-    color: white; /* Cor do texto para contraste com o vídeo */
-    text-align: center;
-    font-family: 'Courier New', Courier, monospace;
-}
-.botao{
-    background-color: blue;
-    color: #ffffff;
-}
-.sql_mostra{
-    margin:auto;
-}
-.registro{
-    margin: auto;
-    background-color: rgba(255, 255, 0, 0.5);
-    color: black;
-}
-.botao_transparente {
-      background-color: transparent;
-      border: none;
-      padding: 0;
-      cursor: pointer;
-    }
-
-    /* Deixa a imagem ajustada no botão */
-    .botao_transparente img {
-      display: block;
-      height: auto;
-    }
-</style>
 
 <?php
     // Define o fuso horário
@@ -113,23 +35,27 @@ a {
     $data_max->modify('+2 months');
     $data_max = $data-> format('Y-m-d');
 ?>
+
 <body>
     <div class="cabecalho">
         <ul class="nav">
             <li class="nav-item">
-                <a class="nav-link" href="..\principal\pagina_client_page.php"><img src="..\imagens_videos\fatia_home.png" alt=""><br> Home</a>
+                <a class="nav-link" href="..\principal\pagina_client_page.php"><img
+                        src="..\imagens_videos\fatia_home.png" alt=""><br> Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#"><img src="..\imagens_videos\pizza_reserva.png" alt=""><br> reserva</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#"><img src="..\imagens_videos\caixa_aberta_login.png" alt=""><br> cardapio</a>
+                <a class="nav-link" href="#"><img src="..\imagens_videos\caixa_aberta_login.png" alt=""><br>
+                    cardapio</a>
             </li>
             <li class="nav-item eu">
                 <a class="nav-link" href="#"><img src="..\imagens_videos\pizza_eu.png" alt=""><br> eu</a>
             </li>
             <li class="nav-item sair">
-                <a class="nav-link" href="..\login\logout_controller.php"><img src="..\imagens_videos\caixa_fechada_cadastro.png" alt=""><br> sair</a>
+                <a class="nav-link" href="..\login\logout_controller.php"><img
+                        src="..\imagens_videos\caixa_fechada_cadastro.png" alt=""><br> sair</a>
             </li>
         </ul>
     </div>
@@ -140,16 +66,16 @@ a {
         </video>
         <div class="conteudo">
             <!-- Aqui você pode adicionar o conteúdo da página que ficará sobre o vídeo -->
-            <form  method="POST" action="inserindo_controller.php">
+            <form method="POST" action="inserindo_controller.php">
                 <h1>reserve uma mesa</h1>
 
 
-                
+
 
                 <label for="">horario: </label>
                 <br>
                 <input name="dados[]" type="time" required min="10:00" max="20:00">
-            
+
                 <br>
                 <label for="">para que dia: </label>
                 <br>
@@ -158,25 +84,26 @@ a {
                 <label for="">mesa para quantos: </label>
                 <br>
                 <select name="dados[]" required>
-                    <option value="" disabled selected >nao selecionado</option>
+                    <option value="" disabled selected>nao selecionado</option>
                     <option value="2">2-pessoa</option>
                     <option value="4">4-pessoa</option>
                     <option value="8">8-pessoa</option>
                 </select>
-                
+
                 <br><br>
-                
+
 
                 <button class="botao" type="submit" name="botao">enviar</button>
                 <br>
-            </form>  
+            </form>
             <?php
             if (isset($_SESSION["error"])) {
                 echo$_SESSION["error"];
             }
             ?>
         </div>
-      </div>
-      
+    </div>
+
 </body>
+
 </html>
