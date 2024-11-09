e82abb451f4edfcc71ab9be16c4913bd30e4a547
Gabriel FV <gabrielvasco906@gmail.com>
2024-11-08 22:10:04 -0300

    fixando os perfis

--- views/alterar/alterar_view.php
+++ views/alterar/alterar_view.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="..\imagens_videos" type="image/x-icon">
     <link rel="shortcut icon" href="..\imagens_videos\pizza_reserva.png" type="image/x-icon">
@@ -34,10 +36,12 @@
     align-items: center;
     background-color: black;
 }
+
 .video-background {
     position: relative;
     width: 100%;
-    height: 100vh; /* Define a altura do vídeo como a altura total da tela */
+    height: 100vh;
+    /* Define a altura do vídeo como a altura total da tela */
     overflow: hidden;
 }
 
@@ -45,41 +49,47 @@
     position: absolute;
     top: 50%;
     left: 50%;
-    min-width: 100%; /* Garante que o vídeo cubra toda a largura */
-    min-height: 100%; /* Garante que o vídeo cubra toda a altura */
+    min-width: 100%;
+    /* Garante que o vídeo cubra toda a largura */
+    min-height: 100%;
+    /* Garante que o vídeo cubra toda a altura */
     width: auto;
     height: auto;
     transform: translate(-50%, -50%);
-    z-index: -1; /* Coloca o vídeo atrás do conteúdo */
+    z-index: -1;
+    /* Coloca o vídeo atrás do conteúdo */
 }
 
 .conteudo {
     position: relative;
-    z-index: 1; /* Garante que o conteúdo fique na frente do vídeo */
-    color: white; /* Cor do texto para contraste com o vídeo */
+    z-index: 1;
+    /* Garante que o conteúdo fique na frente do vídeo */
+    color: white;
+    /* Cor do texto para contraste com o vídeo */
     text-align: center;
     font-family: 'Courier New', Courier, monospace;
 }
 
 
-.registro{
+.registro {
     text-align: center;
     margin: 350px;
     width: auto;
     background-color: rgba(80, 80, 80, 0.8);
 }
-.botao_transparente {
-      background-color: transparent;
-      border: none;
-      padding: 0;
-      cursor: pointer;
-    }
 
-    /* Deixa a imagem ajustada no botão */
-    .botao_transparente img {
-      display: block;
-      height: auto;
-    }
+.botao_transparente {
+    background-color: transparent;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+}
+
+/* Deixa a imagem ajustada no botão */
+.botao_transparente img {
+    display: block;
+    height: auto;
+}
 </style>
 <!--
 <?php
@@ -96,7 +106,7 @@
 -->
 
 <body>
-   
+
     </div>
     <div class="video-background">
         <video autoplay muted loop>
@@ -109,32 +119,36 @@
                 <h1>alterar</h1>
                 <label for="">horario: </label>
                 <br>
-                <input name="dados[]" type="time" value="<?php echo $variavel['horario']?>" required min="10:00" max="20:00">
-            
+                <input name="dados[]" type="time" value="<?php echo $variavel['horario']?>" required min="10:00"
+                    max="20:00">
+
                 <br>
                 <label for="">para que dia: </label>
                 <br>
-                <input name="dados[]" type="date" value="<?php echo $variavel['data_reserva']?>" required min="<?php echo$data_atual;?>" max="<?php echo$data_max;?>">
+                <input name="dados[]" type="date" value="<?php echo $variavel['data_reserva']?>" required
+                    min="<?php echo$data_atual;?>" max="<?php echo$data_max;?>">
                 <br>
                 <label for="">mesa para quantos: </label>
                 <br>
-                <select name="dados[]" required >
-                    <option value="<?php echo $variavel['quantidade']?>"> <?php echo $variavel['quantidade'],"-pessoas";?></option>
+                <select name="dados[]" required>
+                    <option value="<?php echo $variavel['quantidade']?>">
+                        <?php echo $variavel['quantidade'],"-pessoas";?></option>
                     <option value="2">2-pessoa</option>
                     <option value="4">4-pessoa</option>
                     <option value="8">8-pessoa</option>
                 </select>
-                
+
                 <br><br>
-                
+
 
                 <button class="botao" type="submit" name="botao">enviar</button>
                 <br>
                 <button><a href="mostrando_reserva.php">voltar</a></button>
-            </form>  
-            
+            </form>
+
         </div>
-      </div>
-      
+    </div>
+
 </body>
+
 </html>
