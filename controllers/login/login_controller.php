bcb17c0e228a1d8d77e3be3d3e95898606454234
Gabriel FV <gabrielvasco906@gmail.com>
2024-11-17 21:31:41 -0300

    login controller usando classe

--- controllers/login/login_controller.php
+++ controllers/login/login_controller.php
@@ -10,61 +10,68 @@
 <body>
     <?php
 
-/**abrir uma sessao, para armazenar dados do usuario em varias paginas */
-session_start();
-/**pegando no arquivo conexao minha conexao :) */
-include("../../models/conexao.php");
+    /**abrir uma sessao, para armazenar dados do usuario em varias paginas */
+    session_start();
+    /**pegando no arquivo conexao minha conexao :) */
+    class LoginController
+    {
+        private $conexao;
+        private $mysqli;
 
-/**verificando se a conexão foi */
-if($mysqli -> connect_errno){
-    echo"deu nao mano",$mysqli -> connect_errno,$mysqli -> connect_error;
-}
-else {
-    /**pegando minhas variaveis */
-    $dados = $_POST["log"] ;
-    /**vai coloca comando sql aqui nao */
-    $dados[0] = $mysqli -> real_escape_string($dados[0]);
-    $dados[1] = $mysqli -> real_escape_string($dados[1]);
-    /**gerando uma consulta para verifacar se email e senha estao corretos */
-    $sql_code = "SELECT id,email,senha,pessoa_status FROM login WHERE email = '$dados[0]'";
-    /**utilizando parametro query para enviar codigo sql caso nao funcione die */
-    $sql_query = $mysqli -> query($sql_code) or die("voce simplismente nao existe");
-    /**verificando se teve algum retorno do meu select*/
-    $quantidade = $sql_query->num_rows;
-    /**caso sim, entao email e senha aprovados */
-    $variavel =  $sql_query->fetch_assoc();
-    
-    if ($quantidade == 1) {
-        /**variavael ira guardar dados do banco de dados como uma array*/
-        if (!isset($_SESSION)) {
-            session_start();
+        public function __construct()
+        {
+            include("../../models/conexao.php");
+            $this->conexao = new Conexao();
+            $this->mysqli = $this->conexao->conectar();
         }
-        /**usando esse parametro é possivel verificar se a senha criptografada do cliente é veridico, caso for retorna 1 True */
-        if (password_verify($dados[1],$variavel["senha"])) {
-            /**usado para verificar se email ja esta cadastrado no arquivo protect */
-            $_SESSION["email"] = $variavel["email"];
-            $_SESSION["id"] = $variavel["id"];
-            $_SESSION["status"] = $variavel["pessoa_status"];
-            /**separando administrador de cliente */
-            if ($variavel["pessoa_status"] == "admin") {
-                /**enviando pessoa administradora para sua pagina */
-                header("location: ../../views/principal_admin/principal_admin_view.php");
+
+        public function login($dados)
+        {
+            /** Verificando se meus dados estão limpos, se não há código SQL */
+            $dados[0] = $this->mysqli->real_escape_string($dados[0]);
+            $dados[1] = $this->mysqli->real_escape_string($dados[1]);
+
+            /** Verificando se já existe alguém com o mesmo email no site */
+            $sql_code = "SELECT * FROM login WHERE email = '$dados[0]'";
+            $sql_query = $this->mysqli->query($sql_code) or die("Erro na consulta ao banco de dados");
+
+            /** Verificando se teve algum retorno da minha consulta */
+            $quantidade = $sql_query->num_rows;
+
+            /** Caso a consulta for igual a 1, então já existe um email */
+            if ($quantidade == 1) {
+                /** Variável irá guardar dados do banco de dados como um array */
+                $variavel = $sql_query->fetch_assoc();
+
+                if (!isset($_SESSION)) {
+                    session_start();
+                }
+
+                /** Usando esse parâmetro é possível verificar se a senha criptografada do cliente é verídica, caso for retorna 1 True */
+                if (password_verify($dados[1], $variavel["senha"])) {
+                    /** Usado para verificar se email já está cadastrado no arquivo protect */
+                    $_SESSION["email"] = $variavel["email"];
+                    $_SESSION["id"] = $variavel["id"];
+                    $_SESSION["status"] = $variavel["pessoa_status"];
+
+                    /** Separando administrador de cliente */
+                    if ($variavel["pessoa_status"] == "admin") {
+                        /** Enviando pessoa administradora para sua página */
+                        header("location: ../../views/principal_admin/principal_admin_view.php");
+                    } else {
+                        /** Enviar pessoa para página do site */
+                        header("location: ../../views/principal_cliente/principal_client_view.php");
+                    }
+                } else {
+                    echo "Senha incorreta";
+                }
+            } else {
+                /** Caso senha ou email estiver incorreto */
+                echo "<h1>Email ou senha inválido<h1/>";
             }
-            else {
-                /**enviar pessoa para pagina do site */
-                header("location: ../../views/principal_cliente/principal_client_view.php");
-            }    
-        }
-        else {
-            echo "senha incorreta"; 
         }
     }
-    /**caso senha ou email estiver incorreto */
-    else {
-        echo"<h1>email ou senha invalido<h1/>";
-    }
-}
-?>
+    ?>
     <!-- caso algo der errado, voltar para login-->
     <button type="submit"><a href="logout_controller.php">sair</a></button>
 </body>
