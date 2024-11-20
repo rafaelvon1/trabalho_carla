ec02953a46da90b0cc0e4d00dbc50adec6fdf73e
Gabriel FV <gabrielvasco906@gmail.com>
2024-11-19 22:43:55 -0300

    fix: arrumando alguns bugs

--- controllers/login/logout_controller.php
+++ controllers/login/logout_controller.php
@@ -11,7 +11,7 @@ class LogoutController
       session_start();
     }
     session_destroy();
-    header("Location: ../../index.php");
+    header("Location: ../../index.html");
     exit();
   }
 }
