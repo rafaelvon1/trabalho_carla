2fb75455f406f3b40b4c49cb5ebda3fe402cc2b6
bkxinn <kauanbkzin@gmail.com>
2024-11-26 21:37:36 -0300

    finalizado

--- front/pages/cardapio/btncima.js
+++ front/pages/cardapio/btncima.js
@@ -5,28 +5,26 @@ document.getElementById('pizzas').addEventListener('click', function() {
     document.getElementById('doces').style.display = 'none';
     document.getElementById('drink').style.display = 'none';
     document.getElementById('alimentos2').style.display = 'none';
-
+    document.getElementById('salgada').style.display = 'none';
+    document.getElementById('doce').style.display = 'none';
 });
-
-
 document.getElementById('esfiha').addEventListener('click', function() {
     document.getElementById('alimentos2').style.display = 'block';
     document.getElementById('salgada').style.display = 'block';
     document.getElementById('doce').style.display = 'none';  
     document.getElementById('alimentos').style.display = 'none';
     document.getElementById('drink').style.display = 'none';
+    document.getElementById('salgadas').style.display = 'none';
+    document.getElementById('doces').style.display = 'none';
+});
+document.getElementById('bebidas').addEventListener('click', function() {
+    document.getElementById('drink').style.display = 'block';
+    document.getElementById('refri').style.display = 'block';
+    document.getElementById('alimentos2').style.display = 'none';
+    document.getElementById('alimentos').style.display = 'none';
     document.getElementById('suco').style.display = 'none';
     document.getElementById('cerva').style.display = 'none';
     document.getElementById('agua').style.display = 'none';
-
-});
-
-document.getElementById('bebidas').addEventListener('click', function() {
-    document.getElementById('drink').style.display = 'block';
-    document.getElementById('btrefri').style.display = 'block';
-    document.getElementById('alimentos2').style.display = 'none';
-    document.getElementById('alimentos').style.display = 'none';
-   
 });
 
 
