10118cb21f8ae4bab750cca84247dc1c4affdaae
Gabriel FV <gabrielvasco906@gmail.com>
2024-11-27 22:15:15 -0300

    ultimas alterações

--- views/perfils/perfil_cliente/perfil_cliente_view.php
+++ views/perfils/perfil_cliente/perfil_cliente_view.php
@@ -2,6 +2,22 @@
 if (!isset($_SESSION)) {
     session_start();
 }
+
+include("../../../models/conexao.php");
+
+$conexao = new Conexao();
+$mysqli = $conexao->conectar();
+
+if ($mysqli->connect_error) {
+    die("Falha na conexão: " . $mysqli->connect_error);
+}
+
+$id = $_SESSION["id"];
+$sql_cod = "SELECT d.nome, l.email, d.cep, d.cpf, d.telefone FROM dados_usuario d INNER JOIN login l ON d.id_client = l.id WHERE d.id_client = $id";
+
+$sql_query = $mysqli->query($sql_cod) or die("erro");
+
+$variavel = $sql_query->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -11,34 +27,33 @@ if (!isset($_SESSION)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil | Pizzaria </title>
-    <!-- ARRUMAR O ESTILO DEPOIS 
-    <link rel="stylesheet" href="../../assets/styles/style_perfil.css">
-    -->
+    <link rel="stylesheet" href="perfil_cliente.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <header>
-        <h1>Perfil</h1>
+        <nav></nav>
     </header>
     <main>
-        <fieldset>
-            <div class="info">
-                <b>
-                    <p>Informações</p>
-                </b>
-                <a href="#">Editar perfil</a>
-            </div>
-            <hr>
-            <div class="dados">
-                <ul>
-                    <li>Nome: </li>
-                    <li>Email: <?php echo $_SESSION['email']; ?> </li>
-                </ul>
-            </div>
-        </fieldset>
+        <div class="imagem">
+            <img src="../../../images/Logopizza.jpg" alt="Imagem da pessoa">
+            <h1>Olá, <?php echo $variavel['nome']; ?>!</h1>
+        </div>
+        <br><br>
+        <h2>Aqui estão suas Informações</h2>
+        <div class="info">
+            <ul>
+                <li>Nome: <?php echo $variavel['nome']; ?></li>
+                <li>Email: <?php echo $variavel['email']; ?></li>
+                <li>Cpf: <?php echo $variavel['cpf']; ?></li>
+                <li>Cep: <?php echo $variavel['cep']; ?></li>
+                <li>Telefone: <?php echo $variavel['telefone']; ?></li>
+            </ul>
+        </div>
     </main>
     <footer>
-        <a href="../../principal_cliente/principal_client_view.php">Voltar</a>
+        <a href="../../principal_cliente/principal_client_view.php">Clique aqui para voltar </a>
     </footer>
 </body>
 
